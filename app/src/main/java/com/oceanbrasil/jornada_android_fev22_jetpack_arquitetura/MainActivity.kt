12d284cd3cd5fc2b7540ee65ec5558bfcc8b419b
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-11 09:29:31 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/jornada_android_fev22_jetpack_arquitetura/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.jornada_android_fev22_jetpack_arquitetura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
